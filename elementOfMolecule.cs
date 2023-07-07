0d068eb17d3eb2025c1f98a6139d63820c557d8f
JovanP123 <korcagin.jovanp@gmail.com>
2023-07-07 23:40:46 +0200

    klasaZaElementiOdMolekulata

---
+++ elementOfMolecule.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elementOfMolecule : MonoBehaviour
+{
+    public GameObject element;
+    public List<GameObject> pointsTo;
+    public int numberOfConnections;
+}
