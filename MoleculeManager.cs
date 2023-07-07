4ee013d0b482fcb97637ddd7e28bd88f3e828216
JovanP123 <korcagin.jovanp@gmail.com>
2023-07-07 22:10:41 +0200

    EtanProblem

--- MoleculeManager.cs
+++ MoleculeManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoleculeManager : MonoBehaviour
 {
+    [SerializeField]
     public List<GameObject> carbons;
     public List<GameObject> hydrogens;
     public int previousNumOfAtoms;
@@ -12,6 +14,10 @@ public class MoleculeManager : MonoBehaviour
     public int durationInFrames;
     public CameraRotator camRotatingScript;
     public float distanceToMoveAfterAddingAtom;
+    public List<List<elementOfMolecule>> segments = new List<List<elementOfMolecule>>();
+    public TMP_Text nameOfMolecule;
+    public List<string> organicPrefixes;
+    public List<string> organicSuffixes;
 
     public void Update(){
         if ((carbons.Count != previousNumOfAtoms) && !alreadyDoingTheMoving){
@@ -38,5 +44,103 @@ public class MoleculeManager : MonoBehaviour
         }
         alreadyDoingTheMoving = false;
     }
+    public List<elementOfMolecule> trackSegment(elementOfMolecule originPoint){
+        List<elementOfMolecule> segment = new List<elementOfMolecule>();
+        segment.Add(originPoint);
+        elementOfMolecule currentPoint = originPoint;
+        while (true){
+            if ((currentPoint.numberOfConnections==1 || currentPoint.numberOfConnections==0) && currentPoint!=originPoint){
+                segment.Add(currentPoint.pointsTo[0].GetComponent<elementOfMolecule>());
+                currentPoint = currentPoint.pointsTo[0].GetComponent<elementOfMolecule>();
+                continue;
+            }
+            else{
+                return segment;
+            }
+        }
+    }
+    public void nameMolecule(){
+        segments = new List<List<elementOfMolecule>>();
+        segments.Add(trackSegment(carbons[0].GetComponent<elementOfMolecule>()));
+        if (segments.Count == 1){
+            List<GameObject> sp3 = new List<GameObject>();
+            List<GameObject> sp2 = new List<GameObject>();
+            List<GameObject> sp = new List<GameObject>();
+            List<GameObject> h = new List<GameObject>();
+            for (int i=0;i<segments[0].Count;i++){
+                if (segments[0].Count==1){
+                    for (int j=0;j<4;j++){
+                        GameObject neighbouringAtom = segments[0][0].element.GetComponent<AtomProperties>().bonds[j].GetComponent<SpThreeBondOccupation>().connectedTo;
+                        if (neighbouringAtom==null){
+                            StartCoroutine(unableToNameMolecule());
+                            return;
+                        }
+                        else if (neighbouringAtom.GetComponent<AtomProperties>().atomType=="H"){
+                            h.Add(neighbouringAtom);
+                        }
+                    }
+                    sp3.Add(segments[0][0].element);
+                }
+                if (segments[0][i].element.GetComponent<AtomProperties>().atomType=="C(sp3)"){
+                    foreach (GameObject bond in segments[0][i].element.GetComponent<AtomProperties>().bonds){
+                        GameObject neighbouringAtom = bond.GetComponent<SpThreeBondOccupation>().connectedTo;
+                        if (neighbouringAtom == segments[0][i].element){
+                            continue;
+                        }
+                        else if (neighbouringAtom==null){
+                            StartCoroutine(unableToNameMolecule());
+                            return;
+                        }
+                        if (neighbouringAtom.GetComponent<AtomProperties>().atomType=="H"){
+                            h.Add(neighbouringAtom);
+                        }
+                        else if (neighbouringAtom.GetComponent<AtomProperties>().atomType=="C(sp3)"){
+                            Debug.Log("yup");
+                            sp3.Add(neighbouringAtom);
+                        }
+                        else if (neighbouringAtom.GetComponent<AtomProperties>().atomType=="C(sp2)"){
+                            sp2.Add(neighbouringAtom);
+                        }
+                        else if (neighbouringAtom.GetComponent<AtomProperties>().atomType=="C(sp)"){
+                            sp.Add(neighbouringAtom);
+                        }
+                    }
+                }
+            }
+            //Naming the molecule;
+            if (sp2.Count==0 && sp.Count==0){
+                nameOfMolecule.text = organicPrefixes[sp3.Count-1]+organicSuffixes[0];
+            }
+            if (sp2.Count==2 && sp3.Count<=1 && sp.Count==0){
+                nameOfMolecule.text = organicPrefixes[sp3.Count+sp2.Count-1]+organicPrefixes[1];
+            }
+            else if (sp2.Count==2 && sp.Count==0){
+                int indexOfFirstSp2 = 0;
+                int indexOfSecondSp2 = 0;
+                for (int i=0;i<segments[0].Count;i++){
+                    if (segments[0][i].element.GetComponent<AtomProperties>().atomType=="C(sp2)"){
+                        indexOfFirstSp2 = i;
+                        indexOfSecondSp2 = i+1;
+                        break;
+                    }
+                }
+                int winningPosition = indexOfFirstSp2+1;
+                if (indexOfFirstSp2+1<segments[0].Count-indexOfSecondSp2){
+                    winningPosition = indexOfFirstSp2+1;
+                }
+                else if (indexOfFirstSp2+1>segments[0].Count-indexOfSecondSp2){
+                    winningPosition = segments[0].Count-indexOfSecondSp2;
+                }
+                nameOfMolecule.text = organicPrefixes[sp3.Count+sp2.Count-1]+"-"+winningPosition.ToString()+"-"+organicSuffixes[1];
+            }
 
+    }
+    IEnumerator unableToNameMolecule(){
+        nameOfMolecule.text = "UNABLE TO NAME MOLECULE";
+        nameOfMolecule.color = Color.red;
+        yield return new WaitForSeconds(2);
+        nameOfMolecule.text = "";
+        nameOfMolecule.color = Color.white;
+    }
+}
 }
